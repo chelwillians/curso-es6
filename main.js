c66bb56a99686f58c429497740439c6719523a74
Michel Willians <michel_wrq@hotmail.com>
2019-11-06 23:46:31 -0300

    Adicionar pacote no babel para aceitar rest/spread

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert('test');
+class TodoList {
+    constructor(){
+        this.todos = [];
+    }
+
+    addTodo(){
+        this.todos.push('novo todo');
+        console.log(this.todos)
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function (){
+    MinhaLista.addTodo();
+}
