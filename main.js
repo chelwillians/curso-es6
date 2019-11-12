eeadb9a7bf31606e08a1f86b8eb31a651b356496
Michel Willians <michel_wrq@hotmail.com>
2019-11-11 23:26:29 -0300

    Exercicio 5

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
-// exercicio 4
+// exercicio 5
 
-//4.1
-const empresa = {
-    nome: "Rocketseat",
+//5.1 Rest
+const arr = [1, 2, 3, 4, 5, 6];
+
+const [x, ...y] = arr;
+
+//5.2 Spread
+const usuario = {
+    nome: 'Diego',
+    idade: 23,
     endereco: {
-        cidade: "Rio do Sul",
-        estado: "SC"
+        cidade: 'Rio do Sul',
+        uf: 'SC',
+        pais: 'Brasil',
     }
 };
 
-const {
-    nome,
-    endereco: { cidade, estado }
-} = empresa;
-
-console.log(nome);
-console.log(cidade);
-console.log(estado);
-
-//4.2
-
-function mostraInfo({ nome, idade }) {
-    return `${nome} tem ${idade} anos.`;
-}
-
-mostraInfo({ nome: "Diego", idade: 23 });
+const usuario2 = {...usuario, nome: 'Gabriel'};
+const usuario3 = {
+    ...usuario,
+    endereco: {...usuario.endereco, cidade: 'Lontras'}
+};
