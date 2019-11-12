14afdee2e945d97e10676b7674b2eced492f36cd
Michel Willians <michel_wrq@hotmail.com>
2019-11-11 23:17:41 -0300

    Exercicio 4

--- bandle.js
+++ bandle.js
@@ -1,148 +1,29 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-//exercicio 1
-var Usuario =
-/*#__PURE__*/
-function () {
-  function Usuario(email, senha) {
-    _classCallCheck(this, Usuario);
-
-    this.email = email; //Anotando na propriedade da classe
-
-    this.senha = senha; //Anotando na propriedade da classe
+// exercicio 4
+//4.1
+var empresa = {
+  nome: "Rocketseat",
+  endereco: {
+    cidade: "Rio do Sul",
+    estado: "SC"
   }
+};
+var nome = empresa.nome,
+    _empresa$endereco = empresa.endereco,
+    cidade = _empresa$endereco.cidade,
+    estado = _empresa$endereco.estado;
+console.log(nome);
+console.log(cidade);
+console.log(estado); //4.2
 
-  _createClass(Usuario, [{
-    key: "isAdmin",
-    value: function isAdmin() {
-      return this.admin === true;
-    }
-  }]);
+function mostraInfo(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  return "".concat(nome, " tem ").concat(idade, " anos.");
+}
 
-  return Usuario;
-}();
-
-var Admin =
-/*#__PURE__*/
-function (_Usuario) {
-  _inherits(Admin, _Usuario);
-
-  function Admin(email, senha) {
-    var _this;
-
-    _classCallCheck(this, Admin);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(Admin).call(this, email, senha));
-    _this.admin = true;
-    return _this;
-  }
-
-  return Admin;
-}(Usuario);
-
-var User1 = new Usuario("email@teste.com", "senha123");
-var Adm1 = new Admin("email@teste.com", "senha123"); // console.log(User1.isAdmin()); // false
-// console.log(Adm1.isAdmin()); // true
-//exercicio 2
-
-var usuarios = [{
-  nome: 'Diego',
-  idade: 23,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Gabriel',
-  idade: 15,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Lucas',
-  idade: 30,
-  empresa: 'Facebook'
-}]; //2.1
-
-var idades = usuarios.map(function (usuario) {
-  return usuario.idade;
-}); // console.log(idades);
-//2.2
-
-var trabalhadores = usuarios.filter(function (item) {
-  return item.empresa === "Rocketseat" && item.idade >= 18;
-}); // console.log(trabalhadores);
-//2.3
-
-var procura = usuarios.find(function (item) {
-  return item.empresa === "Google";
-}); // console.log(procura);
-//2.4
-
-var unindo = usuarios.map(function (usuario) {
-  return _objectSpread({}, usuario, {
-    idade: usuario.idade * 2
-  });
-}).filter(function (usuario) {
-  return usuario.idade <= 50;
-}); // console.log(unindo);
-//exercicio 3
-// 3.1
-
-var arr = [1, 2, 3, 4, 5];
-arr.map(function (item) {
-  return item + 10;
-}); // 3.2
-// Dica: Utilize uma constante pra function
-
-var usuario = {
-  nome: 'Diego',
+mostraInfo({
+  nome: "Diego",
   idade: 23
-};
-
-var mostraIdade = function mostraIdade(usuario) {
-  return usuario.idade;
-};
-
-mostraIdade(usuario); // 3.3
-// Dica: Utilize uma constante pra function
-
-var nome = "Diego";
-var idade = 23;
-
-var mostraUsuario = function mostraUsuario() {
-  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Diego';
-  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
-  return {
-    nome: nome,
-    idade: idade
-  };
-};
-
-mostraUsuario(nome, idade);
-mostraUsuario(nome); // 3.4
-
-var promise = function promise() {
-  return new Promise(function (resolve, reject) {
-    return resolve();
-  });
-};
+});
--- main.js
+++ main.js
@@ -1,77 +1,27 @@
-//exercicio 1
-class Usuario {
-    constructor(email, senha) {
-        this.email = email; //Anotando na propriedade da classe
-        this.senha = senha; //Anotando na propriedade da classe
-    }
+// exercicio 4
 
-    isAdmin() {
-        return this.admin === true;
+//4.1
+const empresa = {
+    nome: "Rocketseat",
+    endereco: {
+        cidade: "Rio do Sul",
+        estado: "SC"
     }
+};
+
+const {
+    nome,
+    endereco: { cidade, estado }
+} = empresa;
+
+console.log(nome);
+console.log(cidade);
+console.log(estado);
+
+//4.2
+
+function mostraInfo({ nome, idade }) {
+    return `${nome} tem ${idade} anos.`;
 }
 
-class Admin extends Usuario {
-    constructor(email, senha) {
-        super(email, senha);
-        this.admin = true;
-    }
-}
-
-const User1 = new Usuario("email@teste.com", "senha123");
-const Adm1 = new Admin("email@teste.com", "senha123");
-
-// console.log(User1.isAdmin()); // false
-// console.log(Adm1.isAdmin()); // true
-
-//exercicio 2
-const usuarios = [
-    { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
-    { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
-    { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
-];
-
-//2.1
-let idades = usuarios.map(usuario => usuario.idade);
-// console.log(idades);
-
-//2.2
-let trabalhadores = usuarios.filter(function (item) {
-    return item.empresa === "Rocketseat" && item.idade >= 18;
-})
-// console.log(trabalhadores);
-
-//2.3
-let procura = usuarios.find(item => item.empresa === "Google")
-// console.log(procura);
-
-//2.4
-let unindo = usuarios
-    .map(usuario => ({ ...usuario, idade: usuario.idade * 2 }))
-    .filter(usuario => usuario.idade <= 50);
-
-// console.log(unindo);
-
-//exercicio 3
-// 3.1
-const arr = [1, 2, 3, 4, 5];
-arr.map(item => item + 10);
-
-// 3.2
-// Dica: Utilize uma constante pra function
-const usuario = { nome: 'Diego', idade: 23 };
-
-const mostraIdade = usuario => usuario.idade;
-mostraIdade(usuario);
-
-// 3.3
-// Dica: Utilize uma constante pra function
-const nome = "Diego";
-const idade = 23;
-
-const mostraUsuario = (nome = 'Diego', idade = 18) => ({ nome, idade });
-
-mostraUsuario(nome, idade);
-mostraUsuario(nome);
-
-// 3.4
-const promise = () => new Promise((resolve, reject) => resolve())
+mostraInfo({ nome: "Diego", idade: 23 });
