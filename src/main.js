807de64b277101f78adce1c6d6cd9397c4bffa0d
Michel Willians <michel_wrq@hotmail.com>
2019-12-28 20:16:35 -0300

    Exercícios modulo 2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-alert('Testando');
+import ClasseUsuario from './functions';
+
+ClasseUsuario.info();
+
+// 
+
+import { idade } from './functions';
+console.log(idade);
+
+// 
+import Usuario, { idade as IdadeUsuario } from './functions';
+console.log('Idade Usuario: ' + IdadeUsuario);
