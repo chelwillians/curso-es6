414d81de804e88401c82b4534536fa8c83a36a65
Michel Willians <michel_wrq@hotmail.com>
2020-01-02 23:23:56 -0300

    Finalização do último módulo do curso =D

--- src/main.js
+++ src/main.js
@@ -1,35 +1,70 @@
-class App{
-    constructor(){
+import api from './api';
+
+class App {
+    constructor() {
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
         this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
     }
 
     //Registra evento
-    registerHandlers(){
+    registerHandlers() {
 
         //ouve quando teve submit
         this.formEl.onsubmit = (event) => this.addRepository(event);
     }
 
-    addRepository(event) {
+    setLoading(loading = true){
+        if(loading === true){
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando...'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        } else{
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRepository(event) {
         // previne que recarrega
         event.preventDefault();
 
-        this.repositories.push({
-            name: 'Teste',
-            descript: 'Lorem Lorem Lorem Lorem',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-            html_url: '#'
-        });
+        const repoInput = this.inputEl.value;
 
-        this.render();
+        if (repoInput.length === 0)
+            return;
+
+        this.setLoading();
+
+        try {
+            const response = await api.get(`/repos/${repoInput}`);
+
+            const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+            // objeto do elemento
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url
+            });
+
+            this.inputEl.value = "";
+
+            this.render();
+        } catch (error) {
+            alert('Repositório não existe.');
+        }
+
+        this.setLoading(false);
     }
 
-    render(){
+    render() {
         this.listEl.innerHTML = '';
 
         this.repositories.forEach(repo => {
@@ -44,6 +79,7 @@ class App{
 
             let linkEl = document.createElement('a');
             linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
             linkEl.appendChild(document.createTextNode('Acessar'));
 
             let listItemEl = document.createElement('li');
